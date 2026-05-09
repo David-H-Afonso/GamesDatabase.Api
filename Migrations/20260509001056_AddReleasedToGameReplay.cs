847b0e45efd0748836b356db760b82b7dbabb05d
David Hormiga Afonso <davidhormigafonso@gmail.com>
2026-05-09 02:41:53 +0200

    feat: add 'released' column to game_replay table in migration

--- Migrations/20260509001056_AddReleasedToGameReplay.cs
+++ Migrations/20260509001056_AddReleasedToGameReplay.cs
@@ -10,13 +10,19 @@ namespace GamesDatabase.Api.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<string>(
+                name: "released",
+                table: "game_replay",
+                type: "TEXT",
+                nullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "released",
+                table: "game_replay");
         }
     }
 }
